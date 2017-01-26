db05301bcb1d96f58ff376f52be37650e106c376
Alessandro Resta <alessandro.resta@gmail.com>
2017-01-26 14:39:30 +0000

    Init repo with readme file
    Add .gitignore
    
    Add config.yml with Neo4j credentials
    
    Add csv file with data
    
    Add graph image to illustrate documentation
    
    Implement Auth struct and methods to handle Neo4j connection
    
    Add config.go which will load Viper configurations
    
    Implement Neo4go package to load dataset and create graph
    
    Rename image and add it to Readme file
    
    Add auth unit test

---
+++ README.md
@@ -0,0 +1,3 @@
+# NEO4GO
+
+![GitHub Logo](https://github.com/alesr/neo4go/blob/master/docs/image_1.png)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import neo4go "github.com/alesr/neo4go/src"
+
+func main() {
+	neo4go.Load()
+}
